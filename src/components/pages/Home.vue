f59545428bf7ab2872fcb59b16db246966ca0ec9
shoprogram <shoprogramsa@gmail.com>
2021-10-11 20:29:10 +0900

    Home component 移動

---
+++ src/components/pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="home-inner">
+      <div class="home-title">
+        <h1>ホーム</h1>
+      </div>
+      <div class="home-container">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+.home-inner {
+  width: 8rem;
+  margin: 0 auto;
+}
+</style>
